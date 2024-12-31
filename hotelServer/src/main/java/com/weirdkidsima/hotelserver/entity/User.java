03eef48a04ebeb5af3f58f42e85f2bfc5e237a92
Max Stupnikov <61547155+weirdkidsima@users.noreply.github.com>
2024-12-31 17:49:46 +0300

    feat: add jwt util

--- hotelServer/src/main/java/com/weirdkidsima/hotelserver/entity/User.java
+++ hotelServer/src/main/java/com/weirdkidsima/hotelserver/entity/User.java
@@ -3,11 +3,17 @@ package com.weirdkidsima.hotelserver.entity;
 import com.weirdkidsima.hotelserver.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,39 @@ public class User {
     private String name;
 
     private UserRole userRole;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userRole.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
